2bd8c47253b279f03a9d13845b1e8ff6459b0760
radix <georadix00@gmail.com>
2021-01-03 16:49:47 +0900

    Updates

---
+++ precache-manifest.a0b7a53d04d1431a4b65a26f1704b550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab84baed5ed77b42f5ad7e2e8a7c2d0",
+    "url": "/helloradix/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d738003a80352b7d5659",
+    "url": "/helloradix/movie_app_2020/static/css/main.87b9dfc6.chunk.css"
+  },
+  {
+    "revision": "90af64e32e165cae2a76",
+    "url": "/helloradix/movie_app_2020/static/js/2.f211a836.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/helloradix/movie_app_2020/static/js/2.f211a836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d738003a80352b7d5659",
+    "url": "/helloradix/movie_app_2020/static/js/main.dca3049d.chunk.js"
+  },
+  {
+    "revision": "57c0a1439e7d51713f88",
+    "url": "/helloradix/movie_app_2020/static/js/runtime-main.12d12ef3.js"
+  }
+]);
